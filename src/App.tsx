7b5fba30e599208ffa24fc6d069e0607bc8cd7c7
YumYumNyang <khw121699@gmail.com>
2020-09-08 19:04:37 +0900

    base setting

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div>asdsadasd</div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
